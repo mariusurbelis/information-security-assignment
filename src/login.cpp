053912f16361bc5b2b5332c9fed5f60740534aa1
Marius Urbelis <mariusurbelis23@gmail.com>
2019-10-14 22:51:10 +0100

    Database usage changes in main

--- src/login.cpp
+++ src/login.cpp
@@ -66,16 +66,12 @@ void db_parse_line(string line)
   string token;
 
   getline(tokenStream, token, ':');
-
   char *username = (char *)malloc(sizeof(sizeof(char) * token.length()));
-
   strcpy(username, token.c_str());
 
-  cout << token << " " << token.length() << endl;
 
   getline(tokenStream, token, ':');
-  char *password = (char *)malloc(sizeof(token));
-
+  char *password = (char *)malloc(sizeof(char) * token.length());
   strcpy(password, token.c_str());
 
   UserData *ud = (UserData *)malloc(sizeof(UserData));
@@ -113,20 +109,15 @@ int main(int argc, char *argv[])
 
   import_cred_db(DB_FILE_NAME);
 
-  /*
-      usage database[0]->username to access the first username
-  */
-  cout << database[0]->username << endl;
-
   usr_input();
   for (auto i : database)
   {
-    if (!i[0].compare(user_name))
+    if (!strcmp(i->username, user_name.c_str()))
     {
-      cout << i[0] << endl
-           << i[1] << endl
+      cout << i->username << endl
+           << i->password << endl
            << psswd << endl;
-      if (!i[1].compare(psswd))
+      if (!strcmp(i->password, psswd.c_str()))
       {
         authenticated(user_name);
       }
