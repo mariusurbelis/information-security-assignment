caf7a3f566419c07e7f99011d6f44bee4e4497f3
Marius Urbelis <mariusurbelis23@gmail.com>
2019-10-14 12:36:40 +0100

    Some obfuscation

--- src/login.cpp
+++ src/login.cpp
@@ -8,42 +8,52 @@
 #include <vector>
 #include "authlib.h"
 
-using namespace std;
+#define _ ;
+#define __ (
+#define ___ )
+#define ____ int
+#define _____ endl
+#define ______ ,
+#define _______ >>
+#define ________ db_line
+#define _________ user_name
 
-const string DB_FILE_NAME = "pwdb.txt";
-vector<array<string, 2>> database;
-string user_name;
-string psswd;
+using namespace std _
+
+const string DB_FILE_NAME = "pwdb.txt" _
+vector<array<string ______  2 _______  database _
+string  _________  _
+string psswd _
 
 /**
  * Source of hash function: 
  * https://stackoverflow.com/questions/13784434/how-to-use-openssls-sha256-functions#1378448
 */
-string sha256(const string str)
+string sha256 __ const string str ___
 {
-  unsigned char hash[SHA256_DIGEST_LENGTH];
-  SHA256_CTX sha256;
-  SHA256_Init(&sha256);
-  SHA256_Update(&sha256, str.c_str(), str.size());
-  SHA256_Final(hash, &sha256);
-  stringstream ss;
-  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+  unsigned char hash[SHA256_DIGEST_LENGTH] _
+  SHA256_CTX sha256 _
+  SHA256_Init __&sha256 ___ _
+  SHA256_Update __&sha256 ______  str.c_str __ ___ ______  str.size __ ___ ___ _
+  SHA256_Final __ hash ______  &sha256 ___ _
+  stringstream ss _
+  for  __ ____ i = 0 _ i < SHA256_DIGEST_LENGTH _ i++ ___
   {
-    ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    ss << hex << setw __ 2 ___ << setfill __'0' ___ <<  __ ____ ___ hash[i] _
   }
-  return ss.str();
+  return ss.str __ ___ _
 }
 
 /**
- * Function that fulfils the role of interacting with user by taking user input 
+ * Function that fulfils the role of  ____eracting with user by taking user input 
  */
-void usr_input()
+void usr_input __ ___
 {
-  cout << "Enter your username" << endl;
-  cin >> user_name;
-  cout << "Enter your password" << endl;
-  cin >> psswd;
-  psswd = sha256(psswd);
+  cout << "Enter your username" << _____ _
+  cin  _______   _________  _
+  cout << "Enter your password" << _____ _
+  cin  _______  psswd _
+  psswd = sha256 __ psswd ___ _
 }
 
 /**
@@ -54,54 +64,54 @@ void usr_input()
  * 
  *  @param line is the input string
 */
-void db_parse_line(string line)
+void db_parse_line __ string line ___
 {
-  istringstream tokenStream(line);
-  vector<string> tokens;
-  string token;
+  istringstream tokenStream __ line ___ _
+  vector<string> tokens _
+  string token _
 
-  while (getline(tokenStream, token, ':'))
-    tokens.push_back(token);
+  while  __ getline __ tokenStream ______  token ______  ':' ___ ___
+    tokens.push_back __ token ___ _
 
-  database.push_back({tokens[0], tokens[1]});
+  database.push_back __{tokens[0] ______  tokens[1]} ___ _
 }
 
-void import_cred_db(const string db_file_name)
+void import_cred_db __ const string db_file_name ___
 {
-  ifstream db_file(db_file_name);
-  string db_line;
-  if (db_file.is_open())
+  ifstream db_file __ db_file_name ___ _
+  string  ________  _
+  if  __ db_file.is_open __ ___ ___
   {
-    while (getline(db_file, db_line))
+    while  __ getline __ db_file ______   ________  ___ ___
     {
-      db_parse_line(db_line);
+      db_parse_line __  ________  ___ _
     }
-    db_file.close();
+    db_file.close __ ___ _
   }
   else
   {
-    cout << "Unable to read password db file" << endl;
+    cout << "Unable to read password db file" << _____ _
   }
 }
 
-int main()
+ ____ main __ ___
 {
-  import_cred_db(DB_FILE_NAME);
-  usr_input();
-  for (auto i : database)
+  import_cred_db (DB_FILE_NAME);
+  usr_input __ ___ _
+  for  __ auto i : database ___
   {
-    if (!i[0].compare(user_name))
+    if  __!i[0].compare __  _________  ___ ___
     {
-      cout << i[0] << endl
-           << i[1] << endl
-           << psswd << endl;
-      if (!i[1].compare(psswd))
+      cout << i[0] << _____
+           << i[1] << _____
+           << psswd << _____ _
+      if  __!i[1].compare __ psswd ___ ___
       {
-        authenticated(user_name);
+        authenticated __  _________  ___ _
       }
       else
       {
-        rejected(user_name);
+        rejected __  _________  ___ _
       }
     }
   }
